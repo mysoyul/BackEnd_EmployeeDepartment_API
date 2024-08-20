68d65f202d338f1ba5211dd8888dcafde02e537a
mysoyul <vega2k_@naver.com>
2024-08-21 00:56:41 +0900

    boot0.3

--- src/main/java/net/restapi/emp/config/WebConfig.java
+++ src/main/java/net/restapi/emp/config/WebConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 도메인 설정
+                //.allowedOrigins("*") // 허용할 도메인 설정
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드 설정
                 .allowedHeaders("Origin", "Content-Type", "Accept") // 허용할 헤더 설정
                 .allowCredentials(true) // 인증정보 허용 여부
